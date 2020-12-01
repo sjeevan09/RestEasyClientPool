9cf0f7626d5a881982f3a9c709c0d84a3f47df39
sjeevan09 <51773052+sjeevan09@users.noreply.github.com>
2020-12-01 10:38:29 +0530

    Create SampleRestEasyClientPoolApplication.java

---
+++ src/main/java/com/jeevan/rest/easy/SampleRestEasyClientPoolApplication.java
@@ -0,0 +1,13 @@
+package com.jeevan.rest.easy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleRestEasyClientPoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleRestEasyClientPoolApplication.class, args);
+	}
+
+}
